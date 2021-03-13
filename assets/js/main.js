e712d4fd0b51a087334fb00d786a105078cd02e9
Johan Castillo <jcjohan2707@gmail.com>
2021-03-12 23:06:46 -0400

    Dropdown Content

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/*
+#################
+# Elements HTML #
+#################
+*/
+const dropdownMenu = document.getElementById("dropdown-menu")
+
+dropdownMenu.addEventListener("mouseenter", () => console.log("Heey!"))
+
+dropdownMenu.addEventListener("mouseleave", () => console.log("Good bye!"))
+
+
+
+
